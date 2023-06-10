759b94f2cedb96eadfde5192db4636e88fcdcbec
Riceball <42161586+Anderice@users.noreply.github.com>
2023-06-10 11:00:55 +0800

    chore: Setup default style and plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,67 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      'sans-serif': ['Noto Sans CJK TC', 'Noto Sans TC', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       colors: {
         primary: {
-          50: 'rgb(var(--primary-color-50) / <alpha-value>)',
-          100: 'rgb(var(--primary-color-100) / <alpha-value>)',
-          200: 'rgb(var(--primary-color-200) / <alpha-value>)',
-          300: 'rgb(var(--primary-color-300) / <alpha-value>)',
-          400: 'rgb(var(--primary-color-400) / <alpha-value>)',
-          500: 'rgb(var(--primary-color-500) / <alpha-value>)',
-          600: 'rgb(var(--primary-color-600) / <alpha-value>)',
-          700: 'rgb(var(--primary-color-700) / <alpha-value>)',
-          800: 'rgb(var(--primary-color-800) / <alpha-value>)',
-          900: 'rgb(var(--primary-color-900) / <alpha-value>)',
-          1000: 'rgb(var(--primary-color-1000) / <alpha-value>)',
+          50: '#f0f2f2',
+          100: '#e1e4e6',
+          200: '#c4c9cc',
+          300: '#a6aeb3',
+          400: '#899399',
+          500: '#6b7880',
+          600: '#566066',
+          700: '#40484d',
+          800: '#2b3033',
+          900: '#15181a',
         },
+
         accent: {
           DEFAULT: 'rgb(var(--accent-color) / <alpha-value>)',
         },
       },
     },
   },
-  plugins: [require('tailwindcss-fluid-type')],
+  plugins: [
+    require('tailwindcss-fluid-type')({
+      // your fluid type settings
+      // works only with unitless numbers
+      // This numbers are the defaults settings
+      settings: {
+        fontSizeMin: 1.125, // 1.125rem === 18px
+        fontSizeMax: 1.25, // 1.25rem === 20px
+        ratioMin: 1.125, // Multiplicator Min
+        ratioMax: 1.2, // Multiplicator Max
+        screenMin: 20, // 20rem === 320px
+        screenMax: 96, // 96rem === 1536px
+        unit: 'rem', // default is rem but it's also possible to use 'px'
+        prefix: '', // set a prefix to use it alongside the default font sizes
+        extendValues: true, // When you set extendValues to true it will extend the default values. Set it to false to overwrite the values.
+      },
+      // Creates the text-xx classes
+      // This are the default settings and analog to the tailwindcss defaults
+      // Each `lineHeight` is set unitless and we think that's the way to go especially in context with fluid type.
+      values: {
+        xs: [-2, 1.6],
+        sm: [-1, 1.6],
+        base: [0, 1.6],
+        lg: [1, 1.6],
+        xl: [2, 1.2],
+        '2xl': [3, 1.2],
+        '3xl': [4, 1.3],
+        '4xl': [5, 1.4],
+        '5xl': [6, 1.4],
+        '6xl': [7, 1.4],
+        '7xl': [8, 1.4],
+        '8xl': [9, 1],
+        '9xl': [10, 1],
+      },
+    }),
+  ],
 };
