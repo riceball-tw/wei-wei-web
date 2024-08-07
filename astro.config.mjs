513dc7bbaac7f1f9004ab1cbbafaf6072af60f43
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-08-07 20:29:37 +0800

    feat: Add i18n (#6)
    * chore: Add vitest package for unit test
    
    * feat: Add translation files
    
    * feat: Add i18n logics and settings
    
    * feat: Add LanguagePicker & update links in Navbar
    
    * fix: Wrong variable name from defaultLanguage to defaultLocale
    
    * fix: JSON typo
    
    * feat: Update all i18n translation files
    
    * feat: Add work page i18n translation
    
    * feat: Add rel='alternate' for better seo and best practice
    
    * feat: Add i18n to every page and component
    
    * feat: Apply i18n to default locale work page
    
    * feat: Apply i18n to project content collection & move current projects to zh-tw folder
    
    * feat: Remove "titleTC" project property, now I can create different locale project content based on folder
    
    * feat: Add projects i18n translation
    
    * fix: Avoid english translation too long, change translation Work In Progress to WIP
    
    * fix: Current navitem highlight
    
    * refactor: Remove unused function, avoid ci fail
    
    * fix: Build enviroment astro request params cause navbar highlight malfunction, change to other params
    
    * feat: Change i18n icon to global icon
    
    * feat: Tweak LanguagePicker style
    
    * feat(i18n): Tweak zh-tw translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+import { languages, prefixDefaultLocale, defaultLocale } from './src/i18n/i18n.ts';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), mdx()]
+  site: 'https://weweweb.pages.dev',
+  i18n: {
+    defaultLocale,
+    locales: Object.keys(languages),
+    routing: {
+      prefixDefaultLocale,
+    },
+  },
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    mdx(),
+  ],
 });
