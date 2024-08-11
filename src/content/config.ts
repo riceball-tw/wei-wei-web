6ecc75478f313a115afe237492c9fef07ef5a522
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-08-11 17:50:20 +0800

    feat: Add all faq translation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,34 @@
 import { z, defineCollection } from 'astro:content';
 
-const projectCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    draft: z.boolean(),
-    isWorkInProgress: z.boolean().optional(),
-    title: z.string(),
-    description: z.string(),
-    link: z
-      .object({
-        url: z.string().optional(),
-        sourceCodeUrl: z.string().optional(),
-      })
-      .optional(),
-    thumbnail: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    video: z.array(z.object({ src: z.string(), type: z.string() })).optional(),
-    tech: z.array(z.string()).optional(),
-    themeColor: z.string().min(4).max(9).regex(/^#/).default('#fff'),
-    publishDate: z.date(),
-  }),
-});
-
+// eslint-disable-next-line import/prefer-default-export
 export const collections = {
-  project: projectCollection,
+  project: defineCollection({
+    type: 'content',
+    schema: z.object({
+      draft: z.boolean(),
+      isWorkInProgress: z.boolean().optional(),
+      title: z.string(),
+      description: z.string(),
+      link: z
+        .object({
+          url: z.string().optional(),
+          sourceCodeUrl: z.string().optional(),
+        })
+        .optional(),
+      thumbnail: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+      video: z.array(z.object({ src: z.string(), type: z.string() })).optional(),
+      tech: z.array(z.string()).optional(),
+      themeColor: z.string().min(4).max(9).regex(/^#/).default('#fff'),
+      publishDate: z.date(),
+    }),
+  }),
+  faq: defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+    }),
+  }),
 };
