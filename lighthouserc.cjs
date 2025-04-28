031c961300129c49077ec45192d7d67e95eb6552
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-04-28 11:19:11 +0800

    chore: Change lighthouse upload to personal server (#33)
    * chore: Change lighthouse upload to personal server
    
    * chore: Change lighthouse setting from esm to csm based on doc
    
    * chore: Since the whole project is esm based, change back to esm and export by default
    
    * chore: Looks like lighthouse only support csm in doc

---
+++ lighthouserc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      startServerCommand: 'pnpm astro preview',
+      url: [
+        'http://localhost:4321/en/',
+        'http://localhost:4321/en/resume/',
+        'http://localhost:4321/en/work/',
+        'http://localhost:4321/en/faq/',
+      ],
+    },
+    upload: {
+      target: 'lhci',
+      serverBaseUrl: 'https://lhci-weweweb.zeabur.app/',
+      token: process.env.LHCI_TOKEN,
+    },
+  },
+};
