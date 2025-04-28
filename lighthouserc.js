b8e16ed8edb811bca7f50df41819b799d269fbe7
riceball-tw <42161586+riceball-tw@users.noreply.github.com>
2025-04-28 10:47:22 +0800

    chore: Change lighthouse upload to personal server

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/prefer-default-export
+export const config = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      startServerCommand: 'pnpm astro preview',
+      url: [
+        'http://localhost:4321/en/',
+        'http://localhost:4321/en/resume/',
+        'http://localhost:4321/en/work/',
+        'http://localhost:4321/en/faq/',
+      ],
+    },
+    upload: {
+      target: 'lhci',
+      serverBaseUrl: 'https://lhci-weweweb.zeabur.app/',
+      token: process.env.LHCI_TOKEN,
+    },
+  },
+};
