b672ecde0770eceee154ff5d7cac7d0bde11ad4e
riceball-tw <42161586+riceball-tw@users.noreply.github.com>
2025-05-03 13:50:58 +0800

    refactor: Extract og image attributes to global config

--- src/utility/config.ts
+++ src/utility/config.ts
@@ -7,3 +7,10 @@ export const favicons = {
   'android-chrome-512': 'https://bucket-wei.webdong.dev/favicon/android-chrome-512x512.png',
   'apple-touch-icon': 'https://bucket-wei.webdong.dev/favicon/apple-touch-icon.png',
 };
+
+export const globalOgImage = {
+  url: 'https://bucket-wei.webdong.dev/global/og-image.jpg',
+  height: 630,
+  width: 1200,
+  type: 'image/jpeg',
+};
