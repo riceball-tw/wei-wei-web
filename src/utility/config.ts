ec2c967f845c6b2e0f2cc508b8e6fd59d763322d
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-05-27 08:22:44 +0800

    refactor: Extract style images to global config (#40)
    * refactor: Extract global background image to global config
    
    * refactor: Extract global highlight bg image to global config

--- src/utility/config.ts
+++ src/utility/config.ts
@@ -19,6 +19,8 @@ export const website = {
   name: 'WeiWeb',
   email: 'bottle200918@gmail.com',
   emojiStatus: '🍙',
+  globalBackgroundTextureImage: 'https://bucket-wei.webdong.dev/global/natural-paper.webp',
+  globalHighlightTextureImage: 'https://bucket-wei.webdong.dev/global/highlight-texture.webp',
   socials: [
     {
       name: 'Blog',
